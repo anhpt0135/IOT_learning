af14d3174ee98f4d17897a84ee721949fc174b6a
anhpt <pham.anh@hubblehome.com>
2020-02-04 12:03:03 +0700

    Finish Adding https_client with openSSL Hieu duoc cach de su dung code trong sach voi code thuong dung

--- openSSL_https/src/openSSL_https.c
+++ openSSL_https/src/openSSL_https.c
@@ -15,6 +15,12 @@
 #include <netdb.h>
 #include <unistd.h>
 #include <errno.h>
+
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -26,6 +32,8 @@
 #include "openssl/ssl.h"
 #include "openssl/err.h"
 
+
+
 #define MAX_BUFF 1024
 
 void openssl_init(void){
@@ -35,65 +43,168 @@ void openssl_init(void){
 }
 
 int main(int argc, char *argv[]) {
-	const char *server_address = argv[1];//chinh la hostname trong sach
-	const char *port = argv[2];
-	struct sockaddr_in serv_addr;
-	struct addrinfo hints;
-	struct addrinfo *peer_address;
-	int sock = 0;
+#if defined(_WIN32)
+    WSADATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
+        fprintf(stderr, "Failed to initialize.\n");
+        return 1;
+    }
+#endif
 
-	if(getaddrinfo(server_address, port, &hints, &peer_address)){
-		printf("getaddinfo() failed\n");
-		return -1;
-	}
+    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
 
-	puts("!!!OpenSSL_https!!!"); /* prints !!!OpenSSL_https!!! */
-	printf("OpenSSL version: %s\n", OpenSSL_version(SSLEAY_VERSION));
+    SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
+    if (!ctx) {
+        fprintf(stderr, "SSL_CTX_new() failed.\n");
+        return 1;
+    }
 
-	memset(&hints, 0, sizeof(hints));
-	//1. Initialize OPENSSL
-	openssl_init();
-	//tao mot SSL context(mot SSL ctx object) de luu cac setting ban dau
-	SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
-	if(!ctx) {
-		fprintf(stderr, "SSL_CTX_new() failed.\n");
-	}
-	//2. Tao mot TCP connection (TCP socket)
-	if((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0){// Thong tin tu peer_addr co the dem vao day socket(peer_address->ai_family,peer_address->ai_socktype, peer_address->ai_protocol);
-		printf("\n Socket creation error\n");
-		return -1;
-	}
+    if (argc < 3) {
+        fprintf(stderr, "usage: https_simple hostname port\n");
+        return 1;
+    }
 
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(atoi(port));
-
-	if(inet_pton(AF_INET, server_address, &serv_addr.sin_addr) < 0){ // chuyen doi chuoi server_address thanh dia chi ip
-		printf("\n Invalid address\n");
-		return -1;
-	}
-
-	if(connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0 ){
-		printf("\n Connection Failed\n");
-		return 1;
-	}
-	//3. Sau khi TCP connection duoc tao ra thanh cong, tien hanh khoi tao mot TLS connection
-	SSL *ssl = SSL_new(ctx);
-	if(!ctx){
-		printf("SSL_new() failed \n");
-		return 1;
-	}
-
-	if(!SSL_set_tlsext_host_name(ssl, server_address)){
-		printf("SSL_set_tlsext_host_name() failed\n");
-		return 1;
-	}
-
-	SSL_set_fd(ssl, sock);
-	if(SSL_connect(ssl) == -1){
-		printf("SSL_connect() failed\n");
-		return 1;
-	}
+    char *hostname = argv[1];
+    char *port = argv[2];
 
 
-	return EXIT_SUCCESS;
+    printf("Configuring remote address...\n");
+    struct addrinfo hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    struct addrinfo *peer_address;
+    struct sockaddr_in serv_addr;
+    if (getaddrinfo(hostname, port, &hints, &peer_address)) {
+        fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+
+    printf("Remote address is: ");
+    char address_buffer[100];
+    char service_buffer[100];
+    getnameinfo(peer_address->ai_addr, peer_address->ai_addrlen,
+            address_buffer, sizeof(address_buffer),
+            service_buffer, sizeof(service_buffer),
+            NI_NUMERICHOST);
+    printf("%s %s\n", address_buffer, service_buffer);
+
+    printf("Creating socket...\n");
+    SOCKET server;
+/*    server = socket(peer_address->ai_family,
+            peer_address->ai_socktype, peer_address->ai_protocol);*/
+
+    if((server = socket(AF_INET, SOCK_STREAM, 0)) < 0){
+    	printf("Socket creation error\n");
+    	return -1;
+    }
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(port));
+
+    if(inet_pton(AF_INET, address_buffer, &serv_addr.sin_addr) < 0){// chú ý không phải là hostname được truyền vào đây
+    	printf("Invalid address\n");
+    	return -1;
+    }
+
+    if (!ISVALIDSOCKET(server)) {
+        fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }
+
+    printf("Connecting...\n");
+/*    if (connect(server,
+                peer_address->ai_addr, peer_address->ai_addrlen)) {
+        fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
+        exit(1);
+    }*/
+
+    if(connect(server, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
+    	printf("Connection Failed\n");
+    	return -1;
+    }
+
+    freeaddrinfo(peer_address);
+
+    printf("Connected.\n\n");
+
+
+    SSL *ssl = SSL_new(ctx);
+    if (!ctx) {
+        fprintf(stderr, "SSL_new() failed.\n");
+        return 1;
+    }
+
+    if (!SSL_set_tlsext_host_name(ssl, hostname)) {
+        fprintf(stderr, "SSL_set_tlsext_host_name() failed.\n");
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+    SSL_set_fd(ssl, server);
+    if (SSL_connect(ssl) == -1) {
+        fprintf(stderr, "SSL_connect() failed.\n");
+        ERR_print_errors_fp(stderr);
+        return 1;
+    }
+
+    printf ("SSL/TLS using %s\n", SSL_get_cipher(ssl));
+
+
+    X509 *cert = SSL_get_peer_certificate(ssl);
+    if (!cert) {
+        fprintf(stderr, "SSL_get_peer_certificate() failed.\n");
+        return 1;
+    }
+
+    char *tmp;
+    if ((tmp = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0))) {
+        printf("subject: %s\n", tmp);
+        OPENSSL_free(tmp);
+    }
+
+    if ((tmp = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0))) {
+        printf("issuer: %s\n", tmp);
+        OPENSSL_free(tmp);
+    }
+
+    X509_free(cert);
+
+
+    char buffer[2048];
+
+    sprintf(buffer, "GET / HTTP/1.1\r\n");
+    sprintf(buffer + strlen(buffer), "Host: %s:%s\r\n", hostname, port);
+    sprintf(buffer + strlen(buffer), "Connection: close\r\n");
+    sprintf(buffer + strlen(buffer), "User-Agent: https_simple\r\n");
+    sprintf(buffer + strlen(buffer), "\r\n");
+
+    SSL_write(ssl, buffer, strlen(buffer));
+    printf("Sent Headers:\n%s", buffer);
+
+    while(1) {
+        int bytes_received = SSL_read(ssl, buffer, sizeof(buffer));
+            if (bytes_received < 1) {
+                printf("\nConnection closed by peer.\n");
+                break;
+            }
+
+            printf("Received (%d bytes): '%.*s'\n",
+                    bytes_received, bytes_received, buffer);
+
+    } //end while(1)
+
+    printf("\nClosing socket...\n");
+    SSL_shutdown(ssl);
+    CLOSESOCKET(server);
+    SSL_free(ssl);
+    SSL_CTX_free(ctx);
+
+#if defined(_WIN32)
+    WSACleanup();
+#endif
+
+    printf("Finished.\n");
+    return 0;
 }
