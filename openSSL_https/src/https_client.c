ae5f766fa08e7dbad4b018cae312a96d4072118a
anhpt <pham.anh@hubblehome.com>
2020-02-07 17:35:07 +0700

    adding mbedssl

--- openSSL_https/src/https_client.c
+++ openSSL_https/src/https_client.c
@@ -1,12 +1,4 @@
 /*
- * https_client.c
- *
- *  Created on: Feb 4, 2020
- *      Author: anhpt
- */
-
-
-/*
  ============================================================================
  Name        : openSSL_https.c
  Author      : AnhPT
@@ -24,6 +16,11 @@
 #include <unistd.h>
 #include <errno.h>
 
+#define ISVALIDSOCKET(s) ((s) >= 0)
+#define CLOSESOCKET(s) close(s)
+#define SOCKET int
+#define GETSOCKETERRNO() (errno)
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -34,26 +31,295 @@
 #include "openssl/pem.h"
 #include "openssl/ssl.h"
 #include "openssl/err.h"
-
-
+#include "aes.h"
+#include "mbedtls/aes.h"
+#include "mbedtls/md.h"
 
 #define MAX_BUFF 1024
+#define MAX_LENGTH_DATA 1024
+#define IV "00000000000000000000000000000000"
 
-void openssl_init(void){
-	SSL_library_init();//khoi tao the OpenSSL library
-	OpenSSL_add_all_algorithms();// load tat ca cac algorithms san co, co the chi can load nhung thuat toan can thiet thoi
-	SSL_load_error_strings();//de de dang doc cac error messages khi gap loi
+char *key = "00001111222233334444555566667777";
+char *iv = "00000000000000000000000000000000";
+char *data = "EEEEFFFFGGGGHHHH";
+
+unsigned char hex_sha512_key[128] = {0};
+unsigned char sha512_key[64] = {0};
+static unsigned char g_psk[512] = {0};
+
+static int generate_random_number()
+{
+    int number = 0;
+    time_t t;
+
+    srand((unsigned)time(&t));
+    number = rand() % (MAX_LENGTH_DATA * 2);
+
+    return number;
+}
+
+static int unhexify(unsigned char *obuf, const char *ibuf)
+{
+    int len = 0;
+    unsigned char c, c2;
+
+    /* Warning: The length of the input stream have to be an even number */
+    len = (strlen(ibuf) >> 1);
+
+    while(*ibuf != 0)
+    {
+        c = *ibuf++;
+        if(c >= '0' && c <= '9')
+            c -= '0';
+        else if(c >= 'a' && c <= 'f')
+            c -= 'a' - 10;
+        else if(c >= 'A' && c <= 'F')
+            c -= 'A' - 10;
+
+        c2 = *ibuf++;
+        if(c2 >= '0' && c2 <= '9')
+            c2 -= '0';
+        else if(c2 >= 'a' && c2 <= 'f')
+            c2 -= 'a' - 10;
+        else if(c2 >= 'A' && c2 <= 'F')
+            c2 -= 'A' - 10;
+
+        *obuf++ = (c << 4) | c2;
+    }
+
+    return len;
+}
+
+static void hexify(unsigned char *obuf, const unsigned char *ibuf, int len)
+{
+    unsigned char l, h;
+
+    while(len != 0)
+    {
+        h = *ibuf / 16;
+        l = *ibuf % 16;
+
+        if(h < 10)
+            *obuf++ = '0' + h;
+        else
+            *obuf++ = 'a' + h - 10;
+
+        if(l < 10)
+            *obuf++ = '0' + l;
+        else
+            *obuf++ = 'a' + l - 10;
+
+        ++ibuf;
+        len--;
+    }
+}
+
+static int generate_sha512(unsigned char *input, unsigned char *output)
+{
+    int ret = -1;
+    mbedtls_md_context_t sha_ctx;
+
+    if(input == NULL || output == NULL)
+    {
+        printf("Invalid NULL parameter");
+        return -1;
+    }
+
+    if(input[0] == 0x00)
+    {
+        printf("Invalid empty string parameter");
+        return -2;
+    }
+
+    ret = mbedtls_md_setup(&sha_ctx, mbedtls_md_info_from_type(MBEDTLS_MD_SHA512), 1);
+    if(ret != 0)
+    {
+        printf("  ! mbedtls_md_setup() returned -0x%04x\n", -ret);
+        mbedtls_md_free(&sha_ctx);
+        return ret;
+    }
+
+    mbedtls_md_starts(&sha_ctx);
+    mbedtls_md_update(&sha_ctx, input, strlen((char *)input));
+    mbedtls_md_finish(&sha_ctx, output);
+    mbedtls_md_free(&sha_ctx);
+
+    return 0;
+}
+
+static int encrypt_aes128(char *hex_key_string, char *hex_iv_string,
+                          unsigned char *src_string, unsigned char *dst_str, int *encrypt_size)
+{
+    unsigned char key_str[100];
+    unsigned char iv_str[100];
+    unsigned char size_data[20];
+
+    mbedtls_aes_context ctx;
+    int key_len, data_len, cbc_result;
+
+    memset(key_str, 0x00, 100);
+    memset(iv_str, 0x00, 100);
+    memset(size_data, 0x00, 20);
+    mbedtls_aes_init(&ctx);
+
+    key_len = unhexify(key_str, hex_key_string);
+
+    unhexify(iv_str, hex_iv_string);
+
+    data_len = strlen((char *)src_string);
+
+    if(data_len < 1)
+    {
+        printf("Error: encrypt_aes128() length data (%d)", data_len);
+        mbedtls_aes_free(&ctx);
+        return -1;
+    }
+
+    if(data_len % 16 != 0)
+    {
+        data_len = ((data_len / 16) + 1) * 16;
+    }
+
+    if(data_len > MAX_LENGTH_DATA)
+    {
+        printf("Error: encrypt_aes128() length data over (%d)", MAX_LENGTH_DATA);
+        mbedtls_aes_free(&ctx);
+        return -1;
+    }
+
+    sprintf((char *)size_data, "SSL%04d%04d", data_len, generate_random_number());
+
+    mbedtls_aes_setkey_enc(&ctx, key_str, key_len * 8);
+
+    cbc_result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_ENCRYPT, 16, iv_str, size_data, dst_str);
+    if(cbc_result != 0)
+    {
+        printf("Error: encrypt_aes128() length data");
+        mbedtls_aes_free(&ctx);
+        return -2;
+    }
+
+    cbc_result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_ENCRYPT, data_len, iv_str, src_string, dst_str + 16);
+    if(cbc_result != 0)
+    {
+        printf("Error: encrypt_aes128()");
+    }
+
+    mbedtls_aes_free(&ctx);
+    *encrypt_size = (data_len + 16);
+    return cbc_result;
+}
+
+static int decrypt_aes128(char *hex_key_string, char *hex_iv_string,
+                          unsigned char *src_string, unsigned char *dst_str)
+{
+    int random_number;
+    int key_len, data_len, cbc_result, ret = 0;
+    unsigned char key_str[100];
+    unsigned char iv_str[100];
+    unsigned char size_data[20];
+    mbedtls_aes_context ctx;
+
+    if(strlen((char *)src_string) <= 0)
+    {
+        printf("Error decrypt_aes128 length data source");
+        return -1;
+    }
+
+    memset(key_str, 0x00, 100);
+    memset(iv_str, 0x00, 100);
+    memset(size_data, 0x00, 20);
+    mbedtls_aes_init(&ctx);
+
+    key_len = unhexify(key_str, hex_key_string);
+
+    unhexify(iv_str, hex_iv_string);
+
+    mbedtls_aes_setkey_dec(&ctx, key_str, key_len * 8);
+
+    cbc_result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_DECRYPT, 16, iv_str, src_string, size_data);
+    if(cbc_result != 0)
+    {
+        printf("Error: decrypt AES 128");
+        mbedtls_aes_free(&ctx);
+        return -1;
+    }
+
+    ret = sscanf((char *)size_data, "SSL%04d%04d", &data_len, &random_number);
+    if(ret != 2)
+    {
+        printf("Error, cannot parse length data");
+        mbedtls_aes_free(&ctx);
+        return -1;
+    }
+
+    if(data_len % 16 != 0)
+    {
+        data_len = ((data_len / 16) + 1) * 16;
+    }
+
+    if(data_len > MAX_LENGTH_DATA)
+    {
+        printf("Error decrypt_aes128 length data over %d", MAX_LENGTH_DATA);
+        mbedtls_aes_free(&ctx);
+        return -2;
+    }
+
+    cbc_result = mbedtls_aes_crypt_cbc(&ctx, MBEDTLS_AES_DECRYPT, data_len, iv_str, src_string + 16, dst_str);
+    if(cbc_result != 0)
+    {
+        printf("Error decrypt_aes128\n");
+    }
+
+    mbedtls_aes_free(&ctx);
+    return cbc_result;
+}
+
+static void init_psk(char *psk, size_t len, int mode)
+{
+	printf("inside init_psk()\n");
+    char sPSK[64] = {0};
+    char sIdentity[64] = {0};
+    char response_pskidentity[128] ="saurjal0kkcsoduk864r68nv4m";
+    char response_pdkkey[128] = "0C14CB721D6F02D7906AFD641D5BD489";
+
+    if(psk == NULL)
+    {
+        printf("Error: NULL pointer");
+        return;
+    }
+
+    memset(psk, 0x00, len);
+
+    strcpy(sIdentity, response_pskidentity+16);
+    strcpy(sPSK, response_pdkkey+17);
+    printf("Use dynamic Identity & Pre-shared Key\n");
+    snprintf(psk, len, "%s%s", sIdentity, sPSK);
+
+    printf("PSK: %s\n", psk);
+
+    return;
 }
 
 int main(int argc, char *argv[]) {
-	//khoi tao the OpenSSL library
-    openssl_init();
-    // Tao mot OpenSSL context
+
+#if defined(_WIN32)
+    WSADATA d;
+    if (WSAStartup(MAKEWORD(2, 2), &d)) {
+        fprintf(stderr, "Failed to initialize.\n");
+        return 1;
+    }
+#endif
+
+/*    SSL_library_init();
+    OpenSSL_add_all_algorithms();
+    SSL_load_error_strings();
+
     SSL_CTX *ctx = SSL_CTX_new(TLS_client_method());
     if (!ctx) {
         fprintf(stderr, "SSL_CTX_new() failed.\n");
         return 1;
-    }
+    }*/
 
     if (argc < 3) {
         fprintf(stderr, "usage: https_simple hostname port\n");
@@ -63,13 +329,15 @@ int main(int argc, char *argv[]) {
     char *hostname = argv[1];
     char *port = argv[2];
 
-    /***********************Tạo một client TCP socket***************************/
+    init_psk((char*)g_psk, sizeof(g_psk), 1);
+    generate_sha512(g_psk, sha512_key);
+    hexify(hex_sha512_key, sha512_key, 16);
+    printf("[Hex] SHA512 Key:\r\n%s\n", hex_sha512_key);
     printf("Configuring remote address...\n");
     struct addrinfo hints;
     memset(&hints, 0, sizeof(hints));
     hints.ai_socktype = SOCK_STREAM;
     struct addrinfo *peer_address;
-    struct sockaddr_in serv_addr;
     if (getaddrinfo(hostname, port, &hints, &peer_address)) {
         fprintf(stderr, "getaddrinfo() failed. (%d)\n", GETSOCKETERRNO());
         exit(1);
@@ -86,118 +354,66 @@ int main(int argc, char *argv[]) {
 
     printf("Creating socket...\n");
     SOCKET server;
-/*    server = socket(peer_address->ai_family,
-            peer_address->ai_socktype, peer_address->ai_protocol);*/
-
-    if((server = socket(AF_INET, SOCK_STREAM, 0)) < 0){
-    	printf("Socket creation error\n");
-    	return -1;
-    }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(atoi(port));
-
-    if(inet_pton(AF_INET, address_buffer, &serv_addr.sin_addr) < 0){// chú ý không phải là hostname được truyền vào đây
-    	printf("Invalid address\n");
-    	return -1;
-    }
-
+    server = socket(peer_address->ai_family,
+            peer_address->ai_socktype, peer_address->ai_protocol);
     if (!ISVALIDSOCKET(server)) {
         fprintf(stderr, "socket() failed. (%d)\n", GETSOCKETERRNO());
         exit(1);
     }
 
     printf("Connecting...\n");
-/*    if (connect(server,
+    if (connect(server,
                 peer_address->ai_addr, peer_address->ai_addrlen)) {
         fprintf(stderr, "connect() failed. (%d)\n", GETSOCKETERRNO());
         exit(1);
-    }*/
-
-    if(connect(server, (struct sockaddr*)&serv_addr, sizeof(serv_addr)) < 0){
-    	printf("Connection Failed\n");
-    	return -1;
     }
-
     freeaddrinfo(peer_address);
 
-    printf("Connected.\n\n");
-    /******************************************************************/
-    /* Nếu như không cần phải mã hóa, chúng ta có thể giao tiếp thông qua socket vừa tạo ra.
-     * Tuy nhiên chúng ta sẽ dùng OpenSSL để khởi tạo một TLS/SSL connection thông qua TCP connections.
-     * */
-    //Tạo một SSL object từ ctx SSL context
-    SSL *ssl = SSL_new(ctx);
-    if (!ctx) {
-        fprintf(stderr, "SSL_new() failed.\n");
-        return 1;
-    }
-    // (Optional==>This allows OpenSSL to use SNI) giúp cho server biết được certificates của connection hiện tại
-    if (!SSL_set_tlsext_host_name(ssl, hostname)) {
-        fprintf(stderr, "SSL_set_tlsext_host_name() failed.\n");
-        ERR_print_errors_fp(stderr);
-        return 1;
-    }
+    printf("Connected.\n");
 
-    // Hai hàm dưới dùng để khởi tạo new TLS/SSL connection trên TCP socket vừa tạo.
-    SSL_set_fd(ssl, server);
-    if (SSL_connect(ssl) == -1) {
-        fprintf(stderr, "SSL_connect() failed.\n");
-        ERR_print_errors_fp(stderr);
-        return 1;
-    }
+    char buffer[MAX_LENGTH_DATA]={0};
+    unsigned char encrypt_buffer[MAX_LENGTH_DATA] = {0};
+    unsigned char decrypt_buffer[MAX_LENGTH_DATA] = {0};
+    int encrypt_size;
 
-    printf ("SSL/TLS using %s\n", SSL_get_cipher(ssl));
-
-
-    X509 *cert = SSL_get_peer_certificate(ssl);
-    if (!cert) {
-        fprintf(stderr, "SSL_get_peer_certificate() failed.\n");
-        return 1;
-    }
-
-    char *tmp;
-    if ((tmp = X509_NAME_oneline(X509_get_subject_name(cert), 0, 0))) {
-        printf("subject: %s\n", tmp);
-        OPENSSL_free(tmp);
-    }
-
-    if ((tmp = X509_NAME_oneline(X509_get_issuer_name(cert), 0, 0))) {
-        printf("issuer: %s\n", tmp);
-        OPENSSL_free(tmp);
-    }
-
-    X509_free(cert);
-
-
-    char buffer[2048];
-
-    sprintf(buffer, "GET / HTTP/1.1\r\n");
+    sprintf(buffer, "GET /?action=command&command=get_udid HTTP/1.1\r\n");
     sprintf(buffer + strlen(buffer), "Host: %s:%s\r\n", hostname, port);
-    sprintf(buffer + strlen(buffer), "Connection: close\r\n");
+    //sprintf(buffer + strlen(buffer), "Connection: close\r\n");
     sprintf(buffer + strlen(buffer), "User-Agent: https_simple\r\n");
     sprintf(buffer + strlen(buffer), "\r\n");
 
-    SSL_write(ssl, buffer, strlen(buffer));
+    printf("before encrypt !.....");
+    encrypt_aes128((char *)hex_sha512_key, IV, (unsigned char*)buffer, encrypt_buffer, &encrypt_size);
+    printf("encrypt buffer = %s\n", encrypt_buffer);
+    decrypt_aes128((char *)hex_sha512_key, IV, encrypt_buffer, decrypt_buffer);
+    printf("decrypt the encrypt buffer : %s\n", decrypt_buffer);
+    memset(decrypt_buffer, 0, sizeof(decrypt_buffer));
+    printf("encrypt done!.....");
+    //decrypt_aes128((char *)hex_sha512_key, IV, encrypt_buffer, decrypt_buffer);
+    //SSL_write(ssl, buffer, strlen(buffer));
+    int bytes_sent = send(server, buffer, strlen(buffer), 0);
+    printf("Sent %d bytes.\n", bytes_sent);
+
     printf("Sent Headers:\n%s", buffer);
 
     while(1) {
-        int bytes_received = SSL_read(ssl, buffer, sizeof(buffer));
+        //int bytes_received = SSL_read(ssl, buffer, sizeof(buffer));
+    	int bytes_received = recv(server, buffer, MAX_LENGTH_DATA, 0);
             if (bytes_received < 1) {
                 printf("\nConnection closed by peer.\n");
                 break;
             }
-
+            decrypt_aes128((char *)hex_sha512_key, IV, (unsigned char*)buffer, decrypt_buffer);
             printf("Received (%d bytes): '%.*s'\n",
-                    bytes_received, bytes_received, buffer);
+                                bytes_received, bytes_received, decrypt_buffer);
 
     } //end while(1)
 
     printf("\nClosing socket...\n");
-    SSL_shutdown(ssl);
+    //SSL_shutdown(ssl);
     CLOSESOCKET(server);
-    SSL_free(ssl);
-    SSL_CTX_free(ctx);
+    //SSL_free(ssl);
+/*    SSL_CTX_free(ctx);*/
 
 #if defined(_WIN32)
     WSACleanup();
